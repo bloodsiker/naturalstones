ff1be79658c3b4203b345bb9b4fc5c712ac63542
Dmitri Ovsiichuk <maldini2@ukr.net>
2023-03-31 13:37:40 +0300

    Added cron command

--- src/AppBundle/Services/SendTelegramService.php
+++ src/AppBundle/Services/SendTelegramService.php
@@ -328,6 +328,8 @@ class SendTelegramService
             $arrayQuery['message_id'] = $params['message_id'];
         }
 
+        dump($arrayQuery);die;
+
         $ch = curl_init($telegramUrlApi);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $arrayQuery);

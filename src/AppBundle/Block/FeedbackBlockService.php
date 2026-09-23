4d83794599b65df9b668bfec964ea2a2406dae8b
Dmytro Ovsiichuk <dmytro.ovsiichuk@stfalcon.com>
2026-09-24 00:25:09 +0300

    Enabled

--- src/AppBundle/Block/FeedbackBlockService.php
+++ src/AppBundle/Block/FeedbackBlockService.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Block;
 
 use AppBundle\Services\SendTelegramService;
+use Psr\Cache\CacheItemPoolInterface;
 use Sonata\BlockBundle\Block\BlockContextInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -14,11 +15,15 @@ class FeedbackBlockService extends AbstractEditableBlockService
 {
     public const DEFAULT_TEMPLATE = '@App/Block/feedback.html.twig';
     private const HONEYPOT_MIN_SECONDS = 3;
+    private const RATE_LIMIT_WINDOW_SECONDS = 60;
+    private const RATE_LIMIT_MAX_REQUESTS = 3;
+    private const RATE_LIMIT_BLOCK_SECONDS = 600;
 
     public function __construct(
         Environment $twig,
         private readonly RequestStack $request,
         private readonly SendTelegramService $sendTelegramService,
+        private readonly CacheItemPoolInterface $cache,
     ) {
         parent::__construct($twig);
     }
@@ -41,6 +46,10 @@ class FeedbackBlockService extends AbstractEditableBlockService
         $request = $this->request->getCurrentRequest();
 
         if ($request->isXmlHttpRequest()) {
+            if (!$this->registerRequestAndCheckLimit($request->getClientIp())) {
+                return new JsonResponse(['type' => 'error'], Response::HTTP_TOO_MANY_REQUESTS);
+            }
+
             $formTime = (int) $request->get('form_time', 0);
             $honeypot = $request->get('website', '');
 
@@ -48,7 +57,7 @@ class FeedbackBlockService extends AbstractEditableBlockService
                 return new JsonResponse(['type' => 'success']);
             }
 
-//            $this->sendTelegramService->sendFeedback($request);
+            $this->sendTelegramService->sendFeedback($request);
 
             return new JsonResponse(['type' => 'success']);
         }
@@ -59,5 +68,35 @@ class FeedbackBlockService extends AbstractEditableBlockService
             'block' => $block,
             'settings' => array_merge($blockContext->getSettings(), $block->getSettings()),
         ]);
+    }
+
+    private function registerRequestAndCheckLimit(?string $ip): bool
+    {
+        if (null === $ip) {
+            return true;
+        }
+
+        $blockItem = $this->cache->getItem('feedback_form_block_'.md5($ip));
+        if ($blockItem->isHit()) {
+            return false;
+        }
+
+        $countItem = $this->cache->getItem('feedback_form_count_'.md5($ip));
+        $count = $countItem->isHit() ? (int) $countItem->get() : 0;
+        ++$count;
+
+        if ($count > self::RATE_LIMIT_MAX_REQUESTS) {
+            $blockItem->set(true);
+            $blockItem->expiresAfter(self::RATE_LIMIT_BLOCK_SECONDS);
+            $this->cache->save($blockItem);
+
+            return false;
+        }
+
+        $countItem->set($count);
+        $countItem->expiresAfter(self::RATE_LIMIT_WINDOW_SECONDS);
+        $this->cache->save($countItem);
+
+        return true;
     }
 }
